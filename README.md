acd5444b5687d2ee603fae346e43241bf93c760d
Alex <alex@goperigon.com>
2024-08-07 13:58:22 +0300

    Adding main commands

---
+++ README.md
@@ -0,0 +1,1 @@
+# File-s-text-search
---
+++ main/user_interface.py
@@ -0,0 +1,17 @@
+from typing import List
+
+def user_interface():
+    console_user_command: str = None
+    commands_list: List[str] = ["help", "quit"]
+    while True:
+        console_user_command = input("Enter a command: ")
+        match console_user_command:
+            case "help":
+                for command in commands_list:
+                    print(command)
+            case "quit":
+                break
+
+if __name__ == "__main__":
+    print("To see all commands, type 'help'.")
+    user_interface()
